58973c22d528422804b1dbd593fc6297fb9cb8c5
zxqdx <zxq001zxq001@gmail.com>
2015-11-11 11:13:47 -0500

    Added API for single domain.

--- api/controllers/LinkController.js
+++ api/controllers/LinkController.js
@@ -8,17 +8,17 @@
 module.exports = {
   showLink: function(req, res) {
     var from = req.param('from');
-    var to = req.param('from');
+    var to = req.param('to');
     if (!from) {
       return res.json({
         success: false,
         content: 'Must specify from domain!'
       });
     }
-    var singleDomain = !to;
+    var singleDomain = !to || to === '';
+    var rawResults = [];
+    var rawStats = {};
     if (!singleDomain) {
-      var rawResults = [];
-      var rawStats = {};
       async.series([function(callback) {
         Link.find({
           fromDomain: from,
@@ -96,7 +96,7 @@ module.exports = {
           }
         });
         callback();
-      }, function(callback) {
+      }, function() {
         return res.json({
           success: true,
           content: {
@@ -106,11 +106,163 @@ module.exports = {
         });
       }]);
     } else {
-      // TODO
-      return res.json({
-        success: false,
-        content: 'Not implemented yet.'
-      });
+      var domainQueue = [];
+      var domainSet = {
+        from: true
+      };
+      // Graph
+      //   name: domain name
+      //   children: [{
+      //     name: child domain name
+      //     count: # of links from parent node to this child node
+      //     totalToDepth: total toDepth of links that are from parent node to it
+      //     totalFromDepth: total fromDepth of links that are from parent node to it
+      //     children: [{}, ...]
+      //   }, ...]
+      var domainGraph = {
+        name: from,
+        children: []
+      };
+      var tempMap = {};
+      var tempNestedMap = {};
+      // Stats
+      //   totalCount: total # of links
+      //   totalDepth: total link depth length
+      //   totalDomains: total # of unique domains
+      //
+      rawStats = {
+        totalCount: 0,
+        totalToDepth: 0,
+        totalFromDepth: 0,
+        totalDomains: 0
+      };
+      async.series([function(callback) {
+        Link.find({
+          fromDomain: from,
+          toDomain: {
+            '!': from
+          }
+        }, function(err, results) {
+          results.forEach(function(eachLink) {
+            var eachTo = eachLink.toDomain;
+            rawStats.totalCount++;
+            rawStats.totalToDepth += eachLink.toDepth;
+            rawStats.totalFromDepth += eachLink.fromDepth;
+            if (!tempMap.hasOwnProperty(eachTo)) {
+              tempMap[eachTo] = {
+                name: eachTo,
+                count: 0,
+                totalToDepth: 0,
+                totalFromDepth: 0,
+                children: []
+              };
+            }
+            if (domainQueue.indexOf(eachTo) == -1) {
+              domainQueue.push(eachTo);
+            }
+            if (!domainSet.hasOwnProperty(eachTo)) {
+              domainSet[eachTo] = true;
+            }
+            tempMap[eachTo].count++;
+            tempMap[eachTo].totalToDepth += eachLink.toDepth;
+            tempMap[eachTo].totalFromDepth += eachLink.fromDepth;
+          });
+          callback();
+        });
+      }, function(callback) {
+        var taskList = [];
+        for (var i = 0; i < domainQueue.length; i++) {
+          var eachChildName = domainQueue[i];
+          tempNestedMap[eachChildName] = {};
+          taskList.push((function(eachChildName) {
+            return function(callback2) {
+              Link.find({
+                fromDomain: from,
+                toDomain: {
+                  '!': from
+                }
+              }, function(err, results) {
+                results.forEach(function(eachLink) {
+                  var eachTo = eachLink.toDomain;
+                  rawStats.totalCount++;
+                  rawStats.totalToDepth += eachLink.toDepth;
+                  rawStats.totalFromDepth += eachLink.fromDepth;
+                  if (!tempNestedMap[eachChildName].hasOwnProperty(eachTo)) {
+                    tempNestedMap[eachChildName][eachTo] = {
+                      name: eachTo,
+                      count: 0,
+                      totalToDepth: 0,
+                      totalFromDepth: 0
+                    };
+                  }
+                  if (!domainSet.hasOwnProperty(eachTo)) {
+                    domainSet[eachTo] = true;
+                  }
+                  tempNestedMap[eachChildName][eachTo].count++;
+                  tempNestedMap[eachChildName][eachTo].totalToDepth += eachLink.toDepth;
+                  tempNestedMap[eachChildName][eachTo].totalFromDepth += eachLink.fromDepth;
+                });
+                callback2();
+              });
+            };
+          })(eachChildName));
+          taskList.push((function(eachChildName) {
+            return function(callback3) {
+              Link.find({
+                fromDomain: {
+                  '!': from
+                },
+                toDomain: from
+              }, function(err, results) {
+                results.forEach(function(eachLink) {
+                  var eachFrom = eachLink.fromDomain;
+                  rawStats.totalCount++;
+                  rawStats.totalFromDepth += eachLink.toDepth;
+                  rawStats.totalToDepth += eachLink.fromDepth;
+                  if (!tempNestedMap[eachChildName].hasOwnProperty(eachFrom)) {
+                    tempNestedMap[eachChildName][eachFrom] = {
+                      name: eachFrom,
+                      count: 0,
+                      totalToDepth: 0,
+                      totalFromDepth: 0
+                    };
+                  }
+                  if (!domainSet.hasOwnProperty(eachFrom)) {
+                    domainSet[eachFrom] = true;
+                  }
+                  tempNestedMap[eachChildName][eachFrom].count++;
+                  tempNestedMap[eachChildName][eachFrom].totalFromDepth += eachLink.toDepth;
+                  tempNestedMap[eachChildName][eachFrom].totalToDepth += eachLink.fromDepth;
+                });
+                callback3();
+              });
+            };
+          })(eachChildName));
+        }
+        async.series(taskList, function(err) {
+          for (var i = 0; i < domainQueue.length; i++) {
+            var eachChildName = domainQueue[i];
+            for (var eachSubChild in tempNestedMap[eachChildName]) {
+              if (tempNestedMap[eachChildName].hasOwnProperty(eachSubChild)) {
+                tempMap[eachChildName].children.push(tempNestedMap[eachChildName][eachSubChild]);
+              }
+            }
+          }
+          callback();
+        });
+      }, function() {
+        rawStats.totalDomains = Object.keys(domainSet).length;
+        for (var i = 0; i < domainQueue.length; i++) {
+            domainGraph.children.push(tempMap[domainQueue[i]]);
+        }
+        return res.json({
+          success: true,
+          content: {
+            stats: rawStats,
+            graph: domainGraph
+          }
+        });
+      }]);
     }
   }
 };
